cd526dcc04fb6c126dd54e9febe5300639fb22cf
Trần Hữu Nhân <nhanhuutran007@gmail.com>
2026-04-29 03:26:51 +0700

    Đồng bộ code và vendor (đã loại bỏ .env khỏi lịch sử)

--- mvc-project/_legacy/includes/footer.php
+++
@@ -1,7 +0,0 @@
-    </div><!-- /.content-wrapper -->
-
-    <!-- Scripts -->
-    <script src="/mvc-project/vendor/home/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/mvc-project/assets/js/script.js"></script>
-</body>
-</html>
--- mvc-project/_legacy/views/auth/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("Location: /mvc-project/views/auth/login.php");
-exit();
-?>
